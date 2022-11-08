4fdc0e77b837fc0ca9d8b04afce9a654659c12f4
meron <merronmuche@gmail.com>
2022-11-08 14:51:08 +0300

    add created at field

--- researches/models.py
+++ researches/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,3 +7,4 @@ from django.db import models
 class Research(models.Model):
     title = models.CharField(max_length=100)
     content= models.TextField() 
+    created_at = models.DateTimeField(default=timezone.now)
