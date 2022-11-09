01142fb7870c32ec10230d4b3e6358d9d66906df
meron <merronmuche@gmail.com>
2022-11-09 10:24:45 +0300

    Add research to admin

--- researches/admin.py
+++ researches/admin.py
@@ -3,6 +3,5 @@ from django.contrib import admin
 from researches.models import Research
 
 class ResearchAdmin(admin.ModelAdmin):
-
-    list_display = ['title', 'image']
+    list_display = ['id', 'title', 'image']
 admin.site.register(Research,ResearchAdmin)
