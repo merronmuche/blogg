fc73708495a0c91151cb4aef9965fd395b61dfd0
meron <merronmuche@gmail.com>
2022-11-09 12:01:44 +0300

    Foreing key autor

--- researches/admin.py
+++ researches/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from researches.models import Research
+from researches.models import Research, User
 
 class ResearchAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'image']
+    list_display = ['id', 'title', 'author', 'image',]
 admin.site.register(Research,ResearchAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'firstname', 'lastname', 'email']
+
+admin.site.register(User, UserAdmin)
--- researches/models.py
+++ researches/models.py
@@ -4,8 +4,20 @@ from django.utils import timezone
 
 # Create your models here.
 
+class User(models.Model):
+
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    city = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return self.firstname
+
+
 class Research(models.Model):
     title = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
     content= models.TextField() 
     image= models.ImageField(upload_to='images', height_field=None, width_field=None)
     created_at = models.DateTimeField(default=timezone.now)
